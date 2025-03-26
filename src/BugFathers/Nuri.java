b52e830b9135ea630bd51ad2a06de27e65ec51d4
PC <nroztrk17@gmail.com>
2025-03-26 18:28:35 +0300

    satirlara eklem yapıldı

--- src/BugFathers/Nuri.java
+++ src/BugFathers/Nuri.java
@@ -3,5 +3,13 @@ package BugFathers;
 public class Nuri {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Team4 selamlar....");
+        System.out.println("Team4 selamlar....");
+        System.out.println("Team4 selamlar....");
+        System.out.println("Team4 selamlar....");
+        System.out.println("Team4 selamlar....");
+        System.out.println("Team4 selamlar....");
     }
+
 }
