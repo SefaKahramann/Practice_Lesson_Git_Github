f393470615a77635f13475458353ed5e371811b4
SefaKahramannn <sefakahraman0001@gmail.com>
2025-03-26 18:28:59 +0300

    Conflict

--- src/BugFathers/Nuri.java
+++ src/BugFathers/Nuri.java
@@ -3,5 +3,13 @@ package BugFathers;
 public class Nuri {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        String e = "zafer ";
+        String q = "aslan ";
+        String w = "rıfat ";
+        String r = "nuri ";
+        String t = "atilla ";
+        String y = "berk ";
+
+        System.out.println("Hello World 1 :) ");
     }
 }
